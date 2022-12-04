2e2a400333abf9424604cfac9f7bc207eb2b7b2e
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2022-12-04 19:59:02 -0300

    Define user avatar alt

--- src/components/ItemUser/index.js
+++ src/components/ItemUser/index.js
@@ -4,7 +4,11 @@ const ItemUser = ({ user, actions }) => {
   return (
     <li className="item-user">
       <div>
-        <img className="item-user__avatar" src={user.avatar} alt="" />
+        <img
+          className="item-user__avatar"
+          src={user.avatar}
+          alt={user.first_name}
+        />
         <h4 className="item-user__name">{`${user.first_name} ${user.last_name}`}</h4>
         <h5 className="item-user__address">{`${user.address.city}, ${user.address.country}`}</h5>
       </div>
